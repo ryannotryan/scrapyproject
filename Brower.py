937402fedd499e1f3d8b76f09cd1f8e9c510d507
Syazone <mohamad_syazwan3@connect.ite.edu.sg>
2022-02-21 17:55:00 +0800

    Initial Commit

---
+++ Brower.py
@@ -0,0 +1,25 @@
+import requests
+
+url = 'http://www.ite.edu.sg/'
+r = requests.get(url)
+
+print(r.text)
+print("Status code:")
+print("\t *", r.status_code)
+
+h = requests.head(url)
+print("Header:")
+print("**********")
+# To print line by line
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+
+headers = {
+    'User-Agent' : 'Iphone 8'
+}
+# Test it on an external site
+url2 = 'http://httpbin.org/headers'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
+
