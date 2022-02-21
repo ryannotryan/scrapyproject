808fcc99edd65e5683c06135a2a235512f28ebb9
Syazone <mohamad_syazwan3@connect.ite.edu.sg>
2022-02-21 20:52:29 +0800

    Initial Commit

--- TestMyProgram.py
+++ TestMyProgram.py
@@ -1,9 +1,11 @@
 import unittest
 import main
+import Browser
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        r.status_code = 200
 
 if __name__ == '__main__':
     unittest.main()
